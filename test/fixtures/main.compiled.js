57aa4b28acd2e5dc52b96d21961a16ca4ebe145a
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-04-20 18:20:30 +0100

    Export values in fixtures

--- test/fixtures/main.compiled.js
+++ test/fixtures/main.compiled.js
@@ -6,3 +6,14 @@ var x = (function(a) {
 var $__0 = [2, 4],
     a = $__0[0],
     b = $__0[1];
+var other = 'val';
+var $__default = a;
+module.exports = {
+  get other() {
+    return other;
+  },
+  get default() {
+    return $__default;
+  },
+  __esModule: true
+};
--- test/fixtures/main.js
+++ test/fixtures/main.js
@@ -1,2 +1,5 @@
 var x = (a) => a * a;
 var [a, b] = [2, 4];
+
+export var other = 'val';
+export default a;
